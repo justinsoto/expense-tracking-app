580c95426548710a2fc26bd61fec0aacea2b553b
Zehao Wang <wang3205945979@gmail.com>
2025-01-18 17:01:35 -0500

    Update login page

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,14 +1,50 @@
-import Navbar from "../components/navbar.jsx"
-import "../styles/login.css"
-function Login() {
-  return(
-    <>
-      <Navbar></Navbar>
-      <h1>
-        Login
-      </h1>
-    </>
-  )
-}
+import Navbar from "../components/navbar.jsx";
+import SocialLogin from "./components/SocialLogin";
+import InputField from "./components/InputField";
+import "../styles/login.css";
 
-export default Login
+const Login = () => {
+  return (
+    <>
+      {/* Navbar at the top */}
+      <Navbar />
+
+      {/* Main login container */}
+      <div className="login-container">
+        <h2 className="form-title">Log in with</h2>
+
+        {/* Social login options */}
+        <SocialLogin />
+
+        {/* Separator */}
+        <p className="separator">
+          <span>or</span>
+        </p>
+
+        {/* Login form */}
+        <form action="#" className="login-form">
+          <InputField type="email" placeholder="Email address" icon="mail" />
+          <InputField type="password" placeholder="Password" icon="lock" />
+
+          <a href="#" className="forgot-password-link">
+            Forgot password?
+          </a>
+
+          <button type="submit" className="login-button">
+            Log In
+          </button>
+        </form>
+
+        {/* Signup prompt */}
+        <p className="signup-prompt">
+          Don&apos;t have an account?{" "}
+          <a href="#" className="signup-link">
+            Sign up
+          </a>
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Login;
